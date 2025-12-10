f7013b283e9efb3333f7676f1b81711c97dca1e0
Michael Matus <michael@matus.at>
2025-12-10 14:35:10 +0100

    Implement a null filter wheel

---
+++ Bev.Instruments.Thorlabs.FW/NullFilterWheel.cs
@@ -0,0 +1,19 @@
+﻿namespace Bev.Instruments.Thorlabs.FW
+{
+    public class NullFilterWheel : IFilterWheel
+    {
+        public string Name => "Null Filter Wheel";
+
+        public int FilterCount => 0;
+
+        public int GetPosition()
+        {
+            return 0;
+        }
+
+        public void GoToPosition(int position)
+        {
+            // Do nothing
+        }
+    }
+}
